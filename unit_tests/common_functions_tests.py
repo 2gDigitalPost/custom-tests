3ac9d303c85080a264881e5473bc9b0e13548cb5
TStand90 <tstand90@gmail.com>
2015-12-24 11:45:46 -0800

    Unit test for abbreviate_text function.

--- unit_tests/common_functions_tests.py
+++ unit_tests/common_functions_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from common_tools.common_functions import title_case
+from common_tools.common_functions import abbreviate_text, title_case
 
 
 class CommonFunctionsTest(unittest.TestCase):
@@ -9,6 +9,12 @@ class CommonFunctionsTest(unittest.TestCase):
         self.assertEquals('Testing', title_case('Testing'))
         self.assertEquals('Testing', title_case('TESTING'))
         self.assertEquals('This Is A Test', title_case('this is a test'))
+
+    def test_abbreviate_text(self):
+        self.assertEquals('Test...', abbreviate_text('Testing this function.', 4))
+        self.assertEquals('Testing...', abbreviate_text('Testing this function.', 8))
+        self.assertEquals('Testing', abbreviate_text('Testing', 10))
+        self.assertEquals('This is my...', abbreviate_text('This is my example', 10))
 
 
 if __name__ == '__main__':
